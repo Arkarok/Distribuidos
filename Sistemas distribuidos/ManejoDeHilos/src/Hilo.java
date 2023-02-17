20ca5ecb4aece991725f1bd97b464e90f6626141
Arkarok <47455232+Arkarok@users.noreply.github.com>
2023-02-17 17:16:22 -0500

    Actualizacion lectura de bloque

--- Sistemas distribuidos/ManejoDeHilos/src/Hilo.java
+++ Sistemas distribuidos/ManejoDeHilos/src/Hilo.java
@@ -2,25 +2,25 @@ import java.io.IOException;
 
 public class Hilo implements Runnable{
 	
-	int id;
+	String nombre;
 	String url;
 	
-	public Hilo(int id, String url) {
-		this.id=id;
+	public Hilo(String nombre, String url) {
+		this.nombre=nombre;
 		this.url=url;
 	}
 
 	@Override
 	public void run() {
-		//No olvidar cambiar la ruta de google y la de los archivos
-		String headlessChrome = "\"C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe\""
-				+ "--headless --disable-gpu --print-to-pdf=\"C:\\Users\\compumax\\Desktop\\pdfs"+"\\"+id+".pdf"+"\" "+url;
-		
-		//System.out.println("\n la solicitud "+id+" esta utilizando el hilo "+Thread.currentThread().getName());
-		System.out.println("\nEl archivo:"+id+".pdf se encuentra en la direccion:C:\\Users\\compumax\\Desktop\\pdfs");
-		
 		try {
+			//No olvidar cambiar la ruta de google y la de los archivos
+			String headlessChrome = "\"C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe\""
+					+ "--headless --disable-gpu --print-to-pdf=\"C:\\Users\\compumax\\Desktop\\pdfs"+"\\"+nombre+".pdf"+"\" "+url;
+			
+			//System.out.println("\n la solicitud "+id+" esta utilizando el hilo "+Thread.currentThread().getName());
+			System.out.print("\nEl archivo:"+nombre+".pdf se encuentra en la direccion:C:\\Users\\compumax\\Desktop\\pdfs");
             Runtime.getRuntime().exec(headlessChrome);
+            
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
--- Sistemas distribuidos/ManejoDeHilos/src/main.java
+++ Sistemas distribuidos/ManejoDeHilos/src/main.java
@@ -10,16 +10,17 @@ public class main {
 		ExecutorService pool=Executors.newFixedThreadPool(3);
 
 		Scanner en= new Scanner(System.in);
+		Scanner en2= new Scanner(System.in);
 		System.out.println("Digita la cantidad de pdfs que decea crear");
 		int entradas=en.nextInt();
 		System.out.println("Ingresa las urls para realizar el procedimiento de transformacion a formato pdf");
 
 		for (int i = 1; i <= entradas; i++) {
 
-			Scanner en2= new Scanner(System.in);
-			String solicitudes=en2.nextLine();
+			String solicitudesNombre=en2.nextLine();
+			String solicitudesUrl=en2.nextLine();
 
-			pool.execute(new Hilo(i,solicitudes));
+			pool.execute(new Hilo(solicitudesNombre,solicitudesUrl));
 		}
 
 		pool.shutdown();
